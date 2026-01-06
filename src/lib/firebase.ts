6a37078cac961fe7249faa817c6b568a61f8796e
Ran <ranoram18@gmail.com>
2026-01-06 12:03:57 +0200

    intreduced a database of users. when accepting terms data will be added. if the user is in the database he will move straight to dashbord

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,9 @@
 // src/lib/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Database
+import { getFirestore,doc,setDoc,getDoc } from "firebase/firestore"; // Database
 import { getAuth } from "firebase/auth";           // Authentication
 import { getStorage } from "firebase/storage";     // File Storage
+import { create } from "domain";
 
 // Your NEW configuration
 const firebaseConfig = {
@@ -14,7 +15,6 @@ const firebaseConfig = {
   appId: "1:800106331910:web:ad0c9b841b5f420cdb6887",
   measurementId: "G-SHXJ88GTE2"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -22,3 +22,29 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+
+export async function addUser(phoneNumber){
+  const cleanedPhone = phoneNumber.replace(/\D/g, '');
+  try{
+    await setDoc(doc(db, "users", cleanedPhone), 
+    { 
+      phone: cleanedPhone,
+      createdAt: new Date(),
+      firstName:"",
+      lastName:"",
+      mainAdress:"",
+      knownAlergies:[],
+      medicalConditions:[],
+      emergencyContacts:[],
+      isVarified: false
+     });
+  }catch(e){
+    console.error("Error adding document: ", e);
+  }
+}
+export async function findUser(phoneNumber){
+  const cleanedPhone = phoneNumber.replace(/\D/g, '');
+  const docRef = doc(db, "users", cleanedPhone);
+  const docSnap = await getDoc(docRef);
+  return docSnap.exists();
+}
