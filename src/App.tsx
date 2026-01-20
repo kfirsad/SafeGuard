5b063f9999ab998def264b23bf598d17a24bac60
Kfir Sadeh <kfirsad@gmail.com>
2026-01-20 18:18:48 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-          <InstallPrompt />
+      <InstallPrompt />
       <TooltipProvider>
         <Toaster />
         <Sonner />
